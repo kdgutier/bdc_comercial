db1220ad3f1ce1c4335f5efc2156ee9245c4ba06
Kin Diego Salvador Gutierrez Olivares <kin.gtz.olivares@gmail.com>
2018-06-02 16:53:26 -0500

    First commit

---
+++ Utils.R
@@ -0,0 +1,29 @@
+#########################################################################################################
+#
+#                 FEATURE ENGINEERING
+#
+#########################################################################################################
+
+### PACKAGES
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(scales)
+library(TTR)
+library(lubridate)
+
+### LOAD DATA
+com_data <- read_excel("./data/DemandForecast_Challenge (1).xlsx", sheet = "HistoricoVentas") 
+colnames(com_data) <- tolower(colnames(com_data))
+
+### TIME DUMMIES
+com_data <- com_data %>%
+  mutate(month = as.factor(month(com_data$mes)),
+         year = as.factor(year(com_data$mes)),
+         sku = as.factor(sku),
+         subagencia = as.factor(subagencia))
+
+# drop original date
+com_data$mes <- NULL
+
+save(com_data, file="./data/data_baseline.Rda")
