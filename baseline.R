67d4302b987a6cbc4e2a56cc2bc4bd538d6107c0
Kin Diego Salvador Gutierrez Olivares <kin.gtz.olivares@gmail.com>
2018-06-02 21:39:01 -0500

    m

--- baseline.R
+++ baseline.R
@@ -63,6 +63,10 @@
 # sMAPE(baseline_preds, test_data$hectolitros)
 # #sMAPE(xgb_preds, test_data$hectolitros)
 
+list.of.packages <- c("ggplot2", "Rcpp")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 sayHello <- function(){
   print('hello')
 }
